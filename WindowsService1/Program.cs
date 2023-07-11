5599eac1131c7f2c91b773b690f3b69b3ba74513
Matt Beaudin (mbeaudin) <matt.beaudin@quest.com>
2023-07-11 14:14:14 -0400

    initial commit

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.ServiceProcess;
+using System.Timers;
+using System.Text;
+using System.Threading;
+
+namespace longStartupService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+        static void Main()
+        {
+            int int_delay;
+            int.TryParse(ConfigurationManager.AppSettings.Get("startDelay"),out int_delay);
+            
+            Thread.Sleep(int_delay);
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace longStartupService
+{
+    
+    public partial class Service1 : ServiceBase
+    {
+       
+        public Service1()
+        {
+            
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+
+
+        }
+
+        protected override void OnStop()
+        {
+            int int_stop_delay;
+            int.TryParse(ConfigurationManager.AppSettings.Get("stopDelay"), out int_stop_delay);
+
+            Thread.Sleep(int_stop_delay);
+
+        }
+
+       
+    }
+}
